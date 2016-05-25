63ea124cb02fb636c23060e7821f31c5acd9cb04
Lazaros Kazantzis <lakazantzis@gmail.com>
2016-05-26 00:08:18 +0300

    Modified routes to better reflect authentication token proccess.

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,10 @@ Rails.application.routes.draw do
   #constraints subdomain: 'api' do
     scope module: 'api' do
       namespace :v1 do
-        resources :users do
-          get :current_network_users, as: :current_network_users, on: :member
-          # get 'current_network_users/:ssid/:mac_address' => 'users#current_network_users', on: :member
-        end
-        get 'users/unregistered_network_users/:ssid/:mac_address' => 'users#unregistered_network_users', as: :unregistered_network_users
+        resources :users, except: [:index, :update, :destroy]
+        resource :users, only: [:update, :destroy]
+        get 'current_network_users' => 'users#current_network_users', as: :current_network_users
+        get 'unregistered_network_users/:ssid/:mac_address' => 'users#unregistered_network_users', as: :unregistered_network_users
       end
     end
   #end
